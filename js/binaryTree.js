48fae1ff08086fea27d186b7ba1910021cecfebe
Joe Karasek <joseph.karasek@gmail.com>
2016-04-07 14:31:53 -0700

    add working binary tree generator

--- js/binaryTree.js
+++ js/binaryTree.js
@@ -1,8 +1,26 @@
 var Grid = require('./../js/grid.js').Grid;
 var Cell = require('./../js/cell.js').Cell;
 
-function BinaryTree() {}
-
-BinaryTree.prototype.generate(grid) {
-  
+function BinaryTree() {
 }
+
+BinaryTree.prototype.generate = function(grid) {
+  var cells = grid.getAll();
+  cells.forEach(function(cell) {
+    var choices = [];
+    if (cell.neighbors.north) {
+      choices.push(cell.neighbors.north);
+    }
+    if (cell.neighbors.east) {
+      choices.push(cell.neighbors.east);
+    }
+
+    if (choices.length>0) {
+      var choice = choices[ Math.floor((Math.random() * choices.length)) ];
+      cell.link(choice);
+    }
+
+  });
+}
+
+exports.BinaryTree = BinaryTree;
--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,10 +1,13 @@
 var Grid = require('./../js/grid.js').Grid;
 var Cell = require('./../js/cell.js').Cell;
+var BinaryTree = require('./../js/binaryTree.js').BinaryTree;
 
 $(document).ready(function(){
   var myGrid = new Grid();
-  myGrid.setSize(10,10);
+  myGrid.setSize(3,3);
   myGrid.initialize();
   console.log("The Grid", myGrid);
+  var myBinaryTree = new BinaryTree();
+  myBinaryTree.generate(myGrid);
   debugger;
 });
