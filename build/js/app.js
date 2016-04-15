088ee2a51de52c97fc504526e78403d381e59135
Joe Karasek <joseph.karasek@gmail.com>
2016-04-15 16:27:51 -0700

    refactor draw walls out of render

--- build/js/app.js
+++ build/js/app.js
@@ -320,6 +320,7 @@ var Cell = require('./../js/cell.js').Cell;
 function Render() {
 }
 
+
 Render.prototype.flexible = function(grid) {
   // Initialize canvas and drawing tool
   var canvas = document.getElementById('maze');
@@ -332,14 +333,40 @@ Render.prototype.flexible = function(grid) {
   var canvasHeight = 800;
   var rows = grid.rows;
   var cols = grid.cols;
-  var cellWidth = canvasWidth/cols;
-  var cellHeight = canvasHeight/rows;
+  var canvasConfig = {
+    canvasWidth: canvasWidth,
+    canvasHeight: canvasHeight,
+    rows: rows,
+    cols: cols
+  };
 
   // Clear the canvas
   ctx.fillStyle = "white";
   ctx.clearRect(0,0,canvasWidth,canvasHeight);
 
-  // Draw walls
+  // Draw all walls
+  this.drawAllWalls(ctx, cells, canvasConfig);
+  // ctx.fillStyle = "black";
+  // cells.forEach(function(cell) {
+  //   var row = cell.row;
+  //   var col = cell.col;
+  //   // Draw east/west passage
+  //   if(!cell.isLinked(cell.neighbors['east'])) {
+  //     ctx.strokeRect((col+1)*cellWidth,row*cellHeight,1,cellHeight);
+  //   }
+  //   // Draw sout/north passage
+  //   if(!cell.isLinked(cell.neighbors['south'])) {
+  //     ctx.strokeRect(col*cellWidth,(row+1)*cellHeight,cellWidth,1);
+  //   }
+  // });
+}
+
+Render.prototype.drawAllWalls= function(ctx, cells, canvasConfig) {
+
+  var cellWidth = canvasConfig.canvasWidth/canvasConfig.cols;
+  var cellHeight = canvasConfig.canvasHeight/canvasConfig.rows;
+
+  // Draw all walls
   ctx.fillStyle = "black";
   cells.forEach(function(cell) {
     var row = cell.row;
@@ -354,8 +381,6 @@ Render.prototype.flexible = function(grid) {
     }
   });
 }
-
-// Render.prototype.
 
 exports.Render = Render;
 
--- js/render.js
+++ js/render.js
@@ -4,6 +4,7 @@ var Cell = require('./../js/cell.js').Cell;
 function Render() {
 }
 
+
 Render.prototype.flexible = function(grid) {
   // Initialize canvas and drawing tool
   var canvas = document.getElementById('maze');
@@ -16,14 +17,40 @@ Render.prototype.flexible = function(grid) {
   var canvasHeight = 800;
   var rows = grid.rows;
   var cols = grid.cols;
-  var cellWidth = canvasWidth/cols;
-  var cellHeight = canvasHeight/rows;
+  var canvasConfig = {
+    canvasWidth: canvasWidth,
+    canvasHeight: canvasHeight,
+    rows: rows,
+    cols: cols
+  };
 
   // Clear the canvas
   ctx.fillStyle = "white";
   ctx.clearRect(0,0,canvasWidth,canvasHeight);
 
-  // Draw walls
+  // Draw all walls
+  this.drawAllWalls(ctx, cells, canvasConfig);
+  // ctx.fillStyle = "black";
+  // cells.forEach(function(cell) {
+  //   var row = cell.row;
+  //   var col = cell.col;
+  //   // Draw east/west passage
+  //   if(!cell.isLinked(cell.neighbors['east'])) {
+  //     ctx.strokeRect((col+1)*cellWidth,row*cellHeight,1,cellHeight);
+  //   }
+  //   // Draw sout/north passage
+  //   if(!cell.isLinked(cell.neighbors['south'])) {
+  //     ctx.strokeRect(col*cellWidth,(row+1)*cellHeight,cellWidth,1);
+  //   }
+  // });
+}
+
+Render.prototype.drawAllWalls= function(ctx, cells, canvasConfig) {
+
+  var cellWidth = canvasConfig.canvasWidth/canvasConfig.cols;
+  var cellHeight = canvasConfig.canvasHeight/canvasConfig.rows;
+
+  // Draw all walls
   ctx.fillStyle = "black";
   cells.forEach(function(cell) {
     var row = cell.row;
@@ -38,7 +65,5 @@ Render.prototype.flexible = function(grid) {
     }
   });
 }
-
-// Render.prototype.
 
 exports.Render = Render;
